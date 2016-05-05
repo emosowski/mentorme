786dde80e10076cb63144140a228916b7973c556
Emily Osowski <emosowski@gmail.com>
2016-05-05 10:12:14 -0400

    fix after sign in path to user show page, add bootstrap and simple form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    users_path
+  def after_sign_in_path_for(user)
+    user_path(user)
   end
 
   protected
