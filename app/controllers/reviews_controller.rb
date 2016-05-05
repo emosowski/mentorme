c9a416a5f2bbe0dc558c632d114191e83da55e31
Emily Osowski <emosowski@gmail.com>
2016-05-05 17:28:44 -0400

    Add review new and create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @appointment = Appointment.find(params[:appointment_id])
+    @review = Review.new
+  end
+
   def index
     @user = User.find(params[:user_id])
     @reviews = @user.reviews
     @feedback = @user.feedback
   end
 
-  def new
-    @review = Review.new
-  end
 
   def create
     @review = Review.new(review_params)
-
+    # @user = current_user.id
     if @review.save
-      redirect_to '/reviews'
+      redirect_to "/"
     else
       render 'new'
     end
