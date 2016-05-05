c742185f22abdfd434c40d46f0c7110086d4ed95
Emily Osowski <emosowski@gmail.com>
2016-05-04 21:53:34 -0400

    Add student mentor layout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout( :choose_layout, except: :root )
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    users_path
+    user_path
   end
 
   protected
@@ -14,5 +15,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:type])
+  end
+
+  def choose_layout
+    if user_signed_in?
+      current_user.type.downcase
+    end
   end
 end
