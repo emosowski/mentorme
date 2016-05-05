70fa21a39e4459a803dadf2523a259a4153d50db
kevcorper <kevcorper@gmail.com>
2016-05-05 16:07:27 -0400

    add appointment methods

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,4 +6,25 @@ class Appointment < ActiveRecord::Base
   has_many :reviews
   belongs_to :student, class_name: "User"
   belongs_to :mentor, class_name: "User"
+
+  def passed?
+		now = Time.now
+		date.year < now.year || date.month < now.month || date.day < now.day || time.hour < now.hour || time.min < now.minute
+  end
+
+  def open?
+  	self.student_id.empty?
+  end
+
+  def received_review
+  	self.reviews.select{|rev| rev.author_id != self.id }
+  end
+
+  private
+    def date_valid
+	    if self.passed?
+	      errors.add(:model_years, "This date has passed")
+	    end
+	  end
+  end
 end
